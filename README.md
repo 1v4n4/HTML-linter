a72a2150d3336a13e304288adde042eec7a5fb7a
ivana <ivananl@yahoo.com>
2021-05-15 23:39:32 +0200

    Added error reports methods

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HTML-linter
 Linter checks for seven types of errors and returns different messages for each situation along with the number of lines. It runs this set of tests:
-trailing spaces detection
+- trailing spaces detection
 - indentation by two: every line, except for the first and last must be intended with an even number of spaces
 - indentation of first and last (not empty) line: must be zero
 - vertical indentation: must be different by zero or two compared to previous/next line (empty lines are ignored)
--- lib/logic.rb
+++ lib/logic.rb
@@ -3,16 +3,19 @@ module Linter
 
   
   class Check
-    attr_reader :file, :lines, :trailing, :indentation, :unclosed, :last_line, :no_alt_text
+    attr_reader :file, :lines, :trailing, :indentation, :unclosed, :last_line, :no_alt_text, :report, :errors
     def initialize (path)
       @file = path
       @lines = IO.readlines(file)
       @trailing = []
-      @indentation = {:by_two => [], :zero => [], :vertical => []}
-      @unclosed = []
       @last_line = false
+      @indentation = {:by_two => [], :zero => [], :vertical => []}
+      @unclosed = []      
       @no_alt_text = {:missing_alt => [], :empty_alt => []}
+      @errors = 0
+      @report = []
       run_checks
+      errors_count
     end
 
     def trailing_spaces
@@ -40,12 +43,12 @@ module Linter
     
     def indentation_first
       arr = @lines.map {|x| x[/^\s*/].size }
-      indentation[:zero].push('first') if arr.first != 0
+      indentation[:zero].push(1) if arr.first != 0
     end
     
     def indentation_last
       arr = @lines.map {|x| x[/^\s*/].size }
-      indentation[:zero].push('last') if arr[-2] != 0 && !last_line
+      indentation[:zero].push(2) if arr[-2] != 0 && !last_line
     end
     
     def indentation_vertical  
@@ -85,6 +88,9 @@ module Linter
       end           
     end
     
+    private  
+    
+    
     def run_checks
       trailing_spaces
       indentation_two
@@ -94,6 +100,55 @@ module Linter
       open_braces
       empty_line?
       alt_check
-    end    
+      errors_count
+      output
+      sort_report
+    end 
+    
+    def errors_count
+      @errors = @trailing.size + @indentation[:by_two].size + @indentation[:zero].size + @indentation[:vertical].size +
+      @unclosed.size + @no_alt_text[:missing_alt].size  +  @unclosed.size + @no_alt_text[:empty_alt].size
+      @errors+=1 if @last_line 
+    end 
+        
+    def output      
+      @trailing.each {|x| @report.push([x, "Line #{x}: Trailing spaces detected!"])}
+      
+      @report.push([lines.size, "#{} line: Content detected! The last line must be empty"]) if @last_line 
+      
+      @indentation[:by_two].each {|x| @report.push([x, "Line #{x}: The wrong indentation detect! You must indent by two."])}
+      
+      @report.push([1, "Line 1: The wrong indentation detect! This line must have zero indentation."]) if @indentation[:zero].include?(1)
+       
+      @report.push([lines.size-1, "#{lines.size-1} line: The wrong indentation detect! This line must have zero indentation."]) if @indentation[:zero].include?(2)
+      
+      @indentation[:vertical].each {|x| @report.push([x, "Line #{x}: The wrong vertical indentation detect!"])}
+      
+      @unclosed.each {|x| @report.push([x, "Line #{x}: Missing brace detect!"])}
+            
+      @no_alt_text[:missing_alt] {|x| @report.push([x, "Line #{x}: Alt not detected! Add an alt attribute"])}
+      
+      @no_alt_text[:empty_alt] {|x| @report.push([x, "Line #{x}: No alt text detected! Add an valid alt attribute"])}
+    end
+    
+    def sort_report
+      @report.sort!.map! {|x| x[1]}
+      
+      p @report
+    end
+    
+    public
+    
+    def total
+      if @errors ==  0 
+        "
+        
+        NO OFFENSES FOUND!" 
+      else
+        "
+        
+        #{@errors} OFFENSES FOUND!" 
+      end
+    end     
   end
 end
